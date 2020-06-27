e68724f6a37b2c30a0677cd05a84b9f619cd06b8
AntoineRenoux <ant.renoux@gmail.com>
2020-06-28 00:10:55 +0200

    ajout attributs model

--- DefenseByNight-SPA/src/app/_models/character.ts
+++ DefenseByNight-SPA/src/app/_models/character.ts
@@ -1,7 +1,18 @@
 import { Affilate } from './affiliate';
+import { Chronicle } from './Chronicle';
+import { Archetype } from './archetype';
 
 export class Character {
     characterKey: string;
+
+    chronicle: Chronicle;
     name: string;
     sect: Affilate;
+
+    archetype: Archetype;
+    concept: string;
+
+    physicalAttribut: number;
+    socialAttribut: number;
+    mentalAttribut: number;
 }
